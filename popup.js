a160d7438dd9b6707ee4f21da1b25e2984efe0ec
Mehedi Mim <mehedi.hasan@vivasoftltd.com>
2023-06-11 22:02:08 +0600

    Made an extenstion

--- popup.js
+++ popup.js
@@ -1,24 +1,81 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var submitButton = document.getElementById("submitButton");
-    var generateSeedButton = document.getElementById("generateSeedButton");
-    var nameInput = document.getElementById("nameInput");
-    var output = document.getElementById("output");
-  
-    submitButton.addEventListener("click", function() {
-      var name = nameInput.value;
-      output.textContent = "Hello, " + name + "!";
-    });
-  
-    generateSeedButton.addEventListener("click", function() {
-      var generatedSeed = generateSeed();
-      nameInput.value = generatedSeed;
-    });
-  
-    function generateSeed() {
-      // Implement your logic to generate the seed here
-      // Example implementation:
-      var seed = Math.random().toString(36).substr(2, 8);
-      return seed;
-    }
+  var submitButton = document.getElementById("submitButton");
+  var generateSeedButton = document.getElementById("generateSeedButton");
+  var refreshButton = document.getElementById("refreshButton");
+  var nameInput = document.getElementById("nameInput");
+  var output = document.getElementById("output");
+  var enterText = document.getElementById("enterText")
+
+  // Load the seed from local storage if it exists
+  var seed = localStorage.getItem("seed");
+  if (seed) {
+    output.textContent = "Connected..!";
+    submitButton.style.display = "none";
+    generateSeedButton.style.display = "none";
+    refreshButton.style.display = "inline";
+    nameInput.style.display = "none";
+    enterText.style.display = "none";
+  }
+
+  submitButton.addEventListener("click", function() {
+    var name = nameInput.value;
+    output.textContent = "Connecting...";
+
+    // Send the request to the API
+    sendRequest(name);
   });
-  
+
+  generateSeedButton.addEventListener("click", function() {
+    // Send the request to the API
+    var newTabUrl = "http://localhost:3000";
+    chrome.tabs.create({ url: newTabUrl });
+  });
+
+  refreshButton.addEventListener("click", function() {
+    // Clear the seed from local storage
+    localStorage.removeItem("seed");
+
+    // Reset the UI
+    output.textContent = "";
+    submitButton.style.display = "inline";
+    generateSeedButton.style.display = "inline";
+    refreshButton.style.display = "none";
+    nameInput.value = "";
+    nameInput.style.display = "inline";
+    enterText.style.display = "inline";
+  });
+
+
+  function sendRequest(seed) {
+    // Implement your logic to send a GET request to the API endpoint here
+    // Example implementation:
+    var apiUrl = "http://localhost:8080/api/v1/verify-seed?seed=" + encodeURIComponent(seed);
+
+    fetch(apiUrl)
+      .then(response => {
+        if (response.ok) {
+          // Request successful
+          return response.json();
+        } else {
+          // Request failed
+          throw new Error("Failed to send request: " + response.status + " " + response.statusText);
+        }
+      })
+      .then(data => {
+        // Handle the response data
+        output.textContent = "Connected..!";
+        submitButton.style.display = "none";
+        generateSeedButton.style.display = "none";
+        refreshButton.style.display = "inline";
+        nameInput.style.display = "none";
+        enterText.style.display = "none";
+
+        // Save the seed to local storage
+        localStorage.setItem("seed", seed);
+      })
+      .catch(error => {
+        // Handle the error
+        output.textContent = "Connection failed";
+      });
+  }
+});
